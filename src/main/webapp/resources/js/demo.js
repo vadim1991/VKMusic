7bb4ee356221a387dc755e183e0e74e22e90a2c7
Vadym Vlasenko <Vadym_Vlasenko@epam.com>
2016-05-18 11:06:14 +0300

    fix js for old player

--- src/main/webapp/resources/js/demo.js
+++ src/main/webapp/resources/js/demo.js
@@ -17,39 +17,5 @@ var player;
                         music: listTrack
                     });
                     player.init();
-                    $(".aplayer-list").scroll(function(){
-                    console.log($(this)[0].scrollHeight);
-                    console.log($(this).outerHeight());
-                    console.log($(this).scrollTop());
-                        if($(this)[0].scrollHeight - $(this).scrollTop() === $(this).outerHeight()) {
-                                player.option.music = player.option.music.concat(listTrack);
-                                console.log(player);
-                        };
-                    });
                 }
             });
-
-function getTracks() {
-    var listTrack;
-    $.ajax({
-        url: "/tracks",
-        method: "get",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (data) {
-            listTrack = data;
-    )};
-    return listTrack;
-};
-
-function initPlayer(tracks) {
-    player = new APlayer({
-        element: document.getElementById('player4'),
-        narrow: false,
-        autoplay: false,
-        showlrc: false,
-        mutex: true,
-        theme: '#26c17e',
-        music: tracks
-    });
-}
